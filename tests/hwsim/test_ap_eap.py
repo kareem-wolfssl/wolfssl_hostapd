aa1e6dccc63e309c256451fe52efa769041a5f6d
Kareem Abuobeid <kareem@wolfssl.com>
2021-08-23 17:52:31 -0700

    2021 cert update

--- tests/hwsim/test_ap_eap.py
+++ tests/hwsim/test_ap_eap.py
@@ -2711,7 +2711,7 @@ def test_ap_wpa2_eap_ttls_server_cert_hash(dev, apdev):
     """WPA2-Enterprise connection using EAP-TTLS and server certificate hash"""
     check_cert_probe_support(dev[0])
     skip_with_fips(dev[0])
-    srv_cert_hash = "f75a953c1aa9967926525d4d860d1ff7e872f7088782f060768d12aecbd5f25e"
+    srv_cert_hash = "f744d9e47e762de1c0e7208aead2abc5bf0ec937cc9e634dadbb27a15324180e"
     params = hostapd.wpa2_eap_params(ssid="test-wpa2-eap")
     hapd = hostapd.add_ap(apdev[0], params)
     dev[0].connect("test-wpa2-eap", key_mgmt="WPA-EAP", eap="TTLS",
@@ -4117,7 +4117,7 @@ def ocsp_req(outfile):
            "-reqout", outfile,
            '-issuer', 'auth_serv/ca.pem',
            '-sha256',
-           '-serial', '0xD8D3E3A6CBE3CD5F',
+           '-serial', '0xD8D3E3A6CBE3CD69',
            '-no_nonce']
     run_openssl(arg)
     if not os.path.exists(outfile):
